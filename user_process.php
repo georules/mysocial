55ca3bc152a4b8068e092a00e61567d26de3c80e
Geoffery Miller <glmiller@pic.fsu.edu>
2012-11-01 12:53:57 -0400

    hashing info/code credit

--- user_process.php
+++ user_process.php
@@ -8,11 +8,13 @@ $password = $_POST["password"];
 // Log the user in
 $userID = database_user_login($username, $password);
 
-if ($userID == 0)	{
+if ($userID == 0)
+{
 	// This user could not login, send to an error page
 	header('Location: error.php?code=2');
 }
-else	{
+else
+{
 	// Go to the new user's page
 	header('Location: user.php?userID=' . $userID);
 }
